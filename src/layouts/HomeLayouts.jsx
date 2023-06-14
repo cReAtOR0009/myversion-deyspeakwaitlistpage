df663afabb22784aa0dd498bd7e807e7fd14fd54
cReAtOR0009 <epidnugoairdrop@gmail.com>
2023-06-14 15:20:51 +0100

    fix netlify auto deploy

--- src/layouts/HomeLayouts.jsx
+++ src/layouts/HomeLayouts.jsx
@@ -4,7 +4,7 @@ import Footer from '../components/Footer'
 import {useState, useEffect, useRef} from 'react'
 import { motion } from 'framer-motion'
 import { Link, Outlet } from 'react-router-dom'
-import Navbar from '../components/navbar'
+import Navbar from '../components/Navbar'
 import { mascot,deyspeaktext,avatar1,avatar2,avatar3,linkdnicon,instagramicon,twittericon,mobilescreen1,mobilescreen2,mobilescreen3, } from '../assets'
 import { textVariant, buttonVariants, animateImage, containerVariant,} from '../animations';
 
